ee22a1a6f41dabfecaf810cbff8b43a1560b1f1f
mstevenson <mbstevenson96@gmail.com>
2022-09-09 13:24:25 -0400

    initial setup and check for functioning code

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Choose Your Own Adventure was selected as the game option.
+
+// The Adventures of Ace 
+
+// Game state: picture of Ace with title and a description of the issue to be solved (find his ball). Once a player has selected their choice by using buttons a hidden timer will start. This timer will stop once Sad Ace is reached or the game is won. 
+
+// When a player goes from one scene to the next an animation of paw prints and a sound will play. There are 4 scenes total. 
+
+// Each scene will reveal more of the story and allow the player to select an option. If the wrong option is selected then Sad Ace will appear and the player has lost. If the player finds Ace's ball then the player has won.
+
+// Once the game is won the timer will stop and reveal how long it took the player to find Ace's ball. If the player loses the timer will stop and reveal how long it took to disappoint Ace. We don't want to disappoint Ace. 
+
+// Styling will be simple and based on a color pallet. Since I will be using outside pictures and videos I didn't want to take away from that. 
+
+// If I have more time then I will add an optional extension where the player can play ball with Ace. This will only be if I'm not drowning by the time I reach MVP. 
+
+// favicon will be of a paw print
