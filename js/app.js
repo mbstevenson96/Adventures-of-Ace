ed5c52ca1032e2a6fcef6e7a5e46bcef499e4088
mstevenson <mbstevenson96@gmail.com>
2022-09-10 21:01:44 -0400

    add video tag, link to JS, and link appropriate files

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ const storyMessages = [
 ];
 
 //------------------ Variables ------------------------//
-// let winner
 // let timerIntervalId
 // let winTime, min, sec, seconds = 0
 
@@ -18,6 +17,7 @@ let sadAceBtn = document.getElementById('sad-ace')
 let sceneStories = document.getElementById('story')
 let picturesOfAce = document.getElementById('image-ace')
 let title = document.getElementById('title')
+let videosOfAce = document.getElementById('video')
 
 // console.log(helpAceBtn, 'hello');
 // console.log(rejectAceBtn, 'hello');
@@ -61,6 +61,10 @@ function scene1() {
   // console.log('click');
   sceneStories.textContent = storyMessages[0]
     // console.log(sceneStories);
+  
+  picturesOfAce.style = "display: none"
+  videosOfAce.style = "display: block"
+  videosOfAce.volume = .10
 
   helpAceBtn.textContent = 'Go Outside and Look!'
 
@@ -75,6 +79,9 @@ function scene2() {
   sceneStories.textContent = storyMessages[1]
     // console.log(sceneStories);
 
+  videosOfAce.src = "../assets/doorAce.mp4"
+  videosOfAce.volume = .10
+
   helpAceBtn.textContent = 'Open Door!'
 
   sadAceBtn.style = 'display: none'
@@ -86,7 +93,10 @@ function scene2() {
 function scene3() {
   sceneStories.textContent = storyMessages[2]
     // console.log(sceneStories);
-
+  
+  videosOfAce.src = "../assets/AceandSophieBackyard.mp4"
+  videosOfAce.volume = .10
+  
   helpAceBtn.textContent = 'Lets check the area around the rain barrel!'
 
   sadAceBtn.style = "display: default"
@@ -97,6 +107,8 @@ function scene3() {
 }
 
 function winningScene() {
+  videosOfAce.style = "display: none"
+  picturesOfAce.style = "display: default"
   picturesOfAce.src = '../assets/ballAce.jpg'
 
   sceneStories.textContent = storyMessages[3]
@@ -127,22 +139,3 @@ function winningScene() {
 // }
 
 
-
-
-// Choose Your Own Adventure was selected as the game option.
-
-// The Adventures of Ace 
-
-// Game state: picture of Ace with title and a description of the issue to be solved (find his ball). Once a player has selected their choice by using buttons a hidden timer will start. This timer will stop once Sad Ace is reached or the game is won. 
-
-// When a player goes from one scene to the next an animation of paw prints and a sound will play. There are 4 scenes total. 
-
-// Each scene will reveal more of the story and allow the player to select an option. If the wrong option is selected then Sad Ace will appear and the player has lost. If the player finds Ace's ball then the player has won.
-
-// Once the game is won the timer will stop and reveal how long it took the player to find Ace's ball. If the player loses the timer will stop and reveal how long it took to disappoint Ace. We don't want to disappoint Ace. 
-
-// Styling will be simple and based on a color pallet. Since I will be using outside pictures and videos I didn't want to take away from that. 
-
-// If I have more time then I will add an optional extension where the player can play ball with Ace. This will only be if I'm not drowning by the time I reach MVP. 
-
-// favicon will be of a paw print
